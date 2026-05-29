8f48291c2141c236fc1b2f0a90dd8067cfaad9cd
Chris Armstrong <mail@chrisarmstrong.io>
2026-05-29 13:03:51 +0100

    Add payment and due dates to audit log, allow override of due date

--- app/views/case-view/v2/_routes.js
+++ app/views/case-view/v2/_routes.js
@@ -44,27 +44,52 @@ router.all('/application*', (req, res, next) => {
 })
 
 router.post('/application/duly-making', (req, res) => {
-  // On duly making complete update application to in progress and move to task list
+  // Grab the current application
   let applicationToEdit = req.application
+
+  // Setup the payment date from answer or fallback to today
   if (req.session.data['payment-year'] && req.session.data['payment-month'] && req.session.data['payment-day']) {
     paymentDate = req.session.data['payment-year']+'-'+parseInt(req.session.data['payment-month']).toLocaleString(undefined, {minimumIntegerDigits: 2})+'-'+parseInt(req.session.data['payment-day']).toLocaleString(undefined, {minimumIntegerDigits: 2})
   } else {
     paymentDate = new Date().toISOString().slice(0, 10)
   }
 
-  // Setup the object with a new audit log
-  const newAudit = Object.assign({
+  // Setup the due date based on payment date
+  let dueDate = new Date()
+  dueDate.setDate(dueDate.getDate() + 84);
+
+  // Setup the object with payment date
+  const payment = Object.assign({
+    type: 'payment',
+    date: new Date(),
+    payment: paymentDate
+  })
+  // Pass the new status into the audit log
+  applicationToEdit[0].audit.unshift(payment)
+
+  // Setup the object with due date
+  const due = Object.assign({
+    type: 'due',
+    date: new Date(),
+    new: dueDate
+  })
+  // Pass the new status into the audit log
+  applicationToEdit[0].audit.unshift(due)
+
+  // Setup the object with a status
+  const newStatus = Object.assign({
     type: 'status',
+    date: new Date(),
     old: applicationToEdit[0].status,
     new: 'Duly made'
   })
+  // Pass the new status into the audit log
+  applicationToEdit[0].audit.unshift(newStatus)
 
-  // Pass the new log into the audit
-  applicationToEdit[0].audit.unshift(newAudit)
-
-  // Save the new status and payment date
+  // Save the new status and payment/due dates
   applicationToEdit[0].status = 'Duly made'
   applicationToEdit[0].payment = paymentDate
+  applicationToEdit[0].due = dueDate
 
   // Enable success banner and go back to summary
   req.session.data['notification'] = 'true'
@@ -87,6 +112,7 @@ router.get('/application/assign-to-me', (req, res) => {
   // Setup the object with a new owner
   const newOwner = Object.assign({
     type: 'owner',
+    date: new Date(),
     old: applicationToEdit[0].owner || 'unassigned',
     new: 'you'
   })
@@ -98,9 +124,10 @@ router.get('/application/assign-to-me', (req, res) => {
   applicationToEdit[0].owner = 'you'
 
   if (applicationToEdit[0].status != 'In progress') {
-    // Setup the object with a new audit log
+    // Setup the object with a status
     const newStatus = Object.assign({
       type: 'status',
+      date: new Date(),
       old: applicationToEdit[0].status,
       new: 'In progress'
     })
@@ -131,6 +158,7 @@ router.post('/application/assign', (req, res) => {
   // Setup the object with a new owner
   const newOwner = Object.assign({
     type: 'owner',
+    date: new Date(),
     old: applicationToEdit[0].owner || 'unassigned',
     new: owner
   })
@@ -142,9 +170,10 @@ router.post('/application/assign', (req, res) => {
   applicationToEdit[0].owner = owner
 
   if (applicationToEdit[0].status != 'In progress') {
-    // Setup the object with a new audit log
+    // Setup the object with a status
     const newStatus = Object.assign({
       type: 'status',
+      date: new Date(),
       old: applicationToEdit[0].status,
       new: 'In progress'
     })
@@ -159,6 +188,38 @@ router.post('/application/assign', (req, res) => {
   // Enable success banner and go back to summary
   req.session.data['notification'] = 'true'
   req.session.data['assigned'] = 'true'
+  res.redirect('summary')
+})
+
+router.post('/application/override-sla', (req, res) => {
+  // Grab the current application
+  let applicationToEdit = req.application
+
+  // Setup the new date from answer or fallback to today
+  if (req.session.data['sla-year'] && req.session.data['sla-month'] && req.session.data['sla-day']) {
+    dueDate = req.session.data['sla-year']+'-'+parseInt(req.session.data['sla-month']).toLocaleString(undefined, {minimumIntegerDigits: 2})+'-'+parseInt(req.session.data['sla-day']).toLocaleString(undefined, {minimumIntegerDigits: 2})
+  } else {
+    dueDate = new Date().toISOString().slice(0, 10)
+  }
+
+  // Setup the object with new due date
+  const due = Object.assign({
+    type: 'due',
+    date: new Date(),
+    old: applicationToEdit[0].due,
+    new: dueDate,
+    reason: req.session.data['sla-reason']
+  })
+
+  // Pass the new due date into the audit log
+  applicationToEdit[0].audit.unshift(due)
+
+  // Save the new due date
+  applicationToEdit[0].due = dueDate
+
+  // Enable success banner and go back to summary
+  req.session.data['notification'] = 'true'
+  req.session.data['due-date'] = 'true'
   res.redirect('summary')
 })
 
